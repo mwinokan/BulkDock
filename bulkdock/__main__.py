503875c30f0bb7a9dde6a05d99f6fefa60575f20
Max Winokan <mwinokan@me.com>
2024-11-08 16:26:43 +0000

    status command

--- bulkdock/__main__.py
+++ bulkdock/__main__.py
@@ -12,10 +12,151 @@ app = typer.Typer(help=HELP, no_args_is_help=True)
 engine = BulkDock()
 
 
-# @app.command()
-# def status():
-#     """Show the status of BulkDock jobs"""
-#     mrich.title("STATUS GOES HERE")
+def human_timedelta_to_seconds(s):
+
+    values = s.split()
+
+    seconds = 0
+
+    for value in values:
+        if value.endswith("s"):
+            seconds += int(value[:-1])
+        elif value.endswith("m"):
+            seconds += 60 * int(value[:-1])
+        elif value.endswith("h"):
+            seconds += 60 * 60 * int(value[:-1])
+        elif value.endswith("d"):
+            seconds += 24 * 60 * 60 * int(value[:-1])
+
+    return seconds
+
+
+@app.command()
+def status():
+    """Show the status of running BulkDock jobs"""
+
+    import subprocess
+    from rich.table import Table
+    from rich.panel import Panel
+    from richqueue.slurm import combined_df
+    from richqueue.table import color_by_state, COLUMNS
+    from datetime import timedelta
+    from richqueue.tools import human_timedelta  # , human_timedelta_to_seconds
+
+    df = combined_df()
+
+    # filter dataframe
+    df = df[df["name"].str.startswith("BulkDock")]
+    df = df[df["job_state"].isin(["RUNNING", "PENDING"])]
+    df = df[["name", "job_id", "run_time", "standard_output", "job_state"]]
+
+    # create the table
+    table = Table(title="Active BulkDock Jobs", box=None, header_style="")
+
+    table.add_column(**COLUMNS["job_id"])
+    table.add_column("[cyan3 underline]Command", style="cyan3")
+    table.add_column("[cyan3 underline]Target", style="cyan3")
+    table.add_column(**COLUMNS["name"])
+    table.add_column(**COLUMNS["run_time"])
+    table.add_column(**COLUMNS["job_state"])
+    table.add_column(
+        "[cornflower_blue underline]Attempted", justify="right", style="cornflower_blue"
+    )
+    table.add_column(
+        "[bold underline]Progress", justify="right", style="bold cornflower_blue"
+    )
+    table.add_column(
+        "[bold underline]Performance", justify="right", style="bold cornflower_blue"
+    )
+    table.add_column(
+        "[cornflower_blue underline]Remaining", justify="right", style="cornflower_blue"
+    )
+
+    def color_by_fraction(fraction):
+
+        if fraction > 0.75:
+            color = "bright_green"
+        elif fraction > 0.50:
+            color = "bright_yellow"
+        elif fraction > 0.25:
+            color = "dark_orange"
+        else:
+            color = "red"
+
+        return f"[{color}]{fraction*100:.1f} %"
+
+    def color_by_performance(performance):
+
+        if performance < 10:
+            color = "bright_green"
+        elif performance < 20:
+            color = "bright_yellow"
+        elif performance < 60:
+            color = "dark_orange"
+        else:
+            color = "red"
+
+        return f"[{color}]{performance:.2f} s/it"
+
+    for i, row in df.iterrows():
+
+        values = []
+
+        name = row["name"]
+
+        command, target, name = name.split(":")
+        command = command.removeprefix("BulkDock.")
+
+        # calculate placement progress
+
+        grep = [f'grep "Placement task " {row.standard_output} | tail -n 1']
+
+        x = subprocess.Popen(
+            grep, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE
+        )
+
+        output = x.communicate()
+
+        progress = output[0].decode()
+
+        if len(progress) < 15:
+            progress = ""
+
+        else:
+            i, n = progress.split("Placement task ")[-1].split(" ")[0].split("/")
+
+            i = int(i)
+            n = int(n)
+
+            fraction = i / n
+
+            progress = color_by_fraction(fraction)
+
+        # calculate performance
+
+        run_seconds = human_timedelta_to_seconds(row.run_time)
+
+        performance = color_by_performance(run_seconds / i)
+
+        # calculate remaining estimate
+
+        remaining = (n - i) * (run_seconds / i)
+        remaining = human_timedelta(timedelta(seconds=remaining))
+
+        values.append(str(row.job_id))
+        values.append(command)
+        values.append(target)
+        values.append(name)
+        values.append(str(row.run_time))
+        values.append(color_by_state(row.job_state))
+        values.append(str(i))
+        values.append(progress)
+        values.append(performance)
+        values.append(str(remaining))
+
+        table.add_row(*values)
+
+    mrich.print(Panel(table, expand=False))
 
 
 @app.command()
